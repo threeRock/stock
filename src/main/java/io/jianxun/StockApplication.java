7d70b29fb30f1a6f1d786e97d2b00ac52aef6f11
tongtn <versnoontt02@gmail.com>
2017-03-08 17:36:54 +0800

    验证rest 及 页面 测试

--- src/main/java/io/jianxun/StockApplication.java
+++ src/main/java/io/jianxun/StockApplication.java
@@ -2,9 +2,24 @@ package io.jianxun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class StockApplication {
+
+	@RequestMapping("/hello")
+	public @ResponseBody String hello() {
+		return "hello";
+	}
+
+	@RequestMapping("hello/page")
+	public String helloPage() {
+		return "hello/page";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StockApplication.class, args);
--- src/test/java/io/jianxun/StockApplicationTests.java
+++ src/test/java/io/jianxun/StockApplicationTests.java
@@ -1,16 +1,44 @@
 package io.jianxun;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class StockApplicationTests {
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	public void contextLoads() {
+	public void helloIT() {
+		String body = this.restTemplate.getForObject("/hello", String.class);
+		assertThat(body).isEqualTo("hello");
+	}
+
+	@Test
+	public void helloPageIT() throws Exception {
+		this.mockMvc.perform(get("/hello/page")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("hello")));
 	}
 
 }
